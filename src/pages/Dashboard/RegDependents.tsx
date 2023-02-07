25fa1a05af33b8465851dd8e36f4c8002e0a533d
Pedro Jorge <pedrojorge2011@gmail.com>
2023-02-07 20:40:09 -0300

    create Social Media form and Dependents form

--- src/pages/Dashboard/RegDependents.tsx
+++ src/pages/Dashboard/RegDependents.tsx
@@ -1,88 +1,150 @@
-import { Typography } from '@mui/material'
+// import { zodResolver } from '@hookform/resolvers/zod'
+// import { useState } from 'react'
+// import { useForm } from 'react-hook-form'
+// import { z } from 'zod'
+import InputMask from 'react-input-mask'
 
+/* const regDataUserSchema = z.object({
+  fullName: z.string(),
+  birth: z.date(),
+  email: z.string().email(),
+  contatc: z.string(),
+  maritalState: z.string(),
+  schoolDegree: z.string(),
+  genre: z.string(),
+  socialName: z.string().optional(),
+  cpf: z.string(),
+  rg: z.string(),
+  cnh: z.string(),
+  streetAdrress: z.string(),
+  city: z.string(),
+  country: z.string(),
+  cep: z.string(),
+})
+
+type RegDataUserFormInputs = z.infer<typeof regDataUserSchema>
+*/
 export function RegDependents() {
+  /*
+  const { register, handleSubmit, watch } = useForm<RegDataUserFormInputs>({
+    resolver: zodResolver(regDataUserSchema),
+  })
+
+  function handleUserFormSubmit(data: RegDataUserFormInputs) {
+    console.log(data)
+  } */
+
   return (
-    <div>
-      <div className="flex flex-col w-[35%] m-auto mt-10">
-        <Typography className="font-bold">
-          Cadastro de informações {'->'} Dependentes
-        </Typography>
-        <div className="flex p-6 bg-zinc-400 rounded-lg border-b-2-cyan-500">
-          <form className="flex flex-col gap-3 w-full">
-            <div className="flex flex-col gap-2">
-              <label htmlFor="nome" className="font-semibold">
-                Nome completo
-              </label>
-              <input
-                type="text"
-                id="nome"
-                name="nome"
-                placeholder="Antonio Levi Parente"
-                className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-              />
+    <>
+      <div className="md:grid md:grid-cols-3 md:gap-6 white py-4">
+        <div className="md:col-span-1">
+          <div className="px-4 sm:p-4">
+            <h3 className="text-lg font-bold leading-6 text-gray-800">
+              Cadastrar dados de dependentes
+            </h3>
+            <p className="mt-1 text-sm font-medium text-gray-700">
+              Confira os dados antes de cadastrar.
+              {/* <pre className="flex flex-wrap">
+                {JSON.stringify(watch(), null, 2)}
+              </pre> */}
+            </p>
+          </div>
+        </div>
+        <div className="mt-5 md:col-span-2">
+          <form>
+            <div className="overflow-hidden shadow sm:rounded-md mr-4">
+              <div className="bg-white px-4 py-5 sm:p-6">
+                <div className="grid grid-cols-6 gap-6 mx-auto">
+                  <div className="col-span-6 sm:col-span-6">
+                    <label
+                      htmlFor="dependent-name"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Nome do dependente
+                    </label>
+                    <input
+                      type="text"
+                      id="dependent-name"
+                      autoComplete="dependent-name"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                    />
+                  </div>
+                  <div className="col-span-6 md:col-span-3">
+                    <label
+                      htmlFor="dependent-birth"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Data de nascimento
+                    </label>
+                    <input
+                      type="date"
+                      id="dependent-birth"
+                      autoComplete="dependent-birth"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="emergency-contact"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Contato
+                    </label>
+                    <InputMask
+                      mask="(99) 99999-9999"
+                      type="text"
+                      id="emergency-contact"
+                      autoComplete="emergency-contact"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                    />
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="covid-code"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      CPF
+                    </label>
+                    <InputMask
+                      mask="999.999.999.99"
+                      type="text"
+                      id="covid-code"
+                      autoComplete="covid-code"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="covid-code"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      RG
+                    </label>
+                    <input
+                      type="text"
+                      id="covid-code"
+                      autoComplete="covid-code"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      maxLength={11}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="bg-zinc-50 px-4 py-3 text-right sm:px-6">
+                <button
+                  type="submit"
+                  className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                >
+                  Cadastrar
+                </button>
+              </div>
             </div>
-            <div className="grid grid-cols-2 gap-4">
-              <div className="flex flex-col gap-2">
-                <label htmlFor="data_nasc" className="font-semibold">
-                  Data de nascimento
-                </label>
-                <input
-                  type="date"
-                  id="data_nasc"
-                  name="data_nasc"
-                  className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <label htmlFor="contato" className="font-semibold">
-                  Contato
-                </label>
-                <input
-                  type="text"
-                  id="contato"
-                  name="contato"
-                  placeholder="(00) 00000-0000"
-                  className="bg-zinc-900 py-1 px-2 rounded text-sm placeholder:text-zinc-400"
-                />
-              </div>
-            </div>
-            <div className="grid grid-cols-2 gap-4">
-              <div className="flex flex-col gap-2">
-                <label htmlFor="rg" className="font-semibold">
-                  RG
-                </label>
-                <input
-                  type="text"
-                  id="rg"
-                  name="rg"
-                  placeholder="0000000000-0"
-                  className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <label htmlFor="cpf" className="font-semibold">
-                  CPF
-                </label>
-                <input
-                  type="text"
-                  id="cpf"
-                  name="cpf"
-                  placeholder="000.000.000-00"
-                  className="bg-zinc-900 py-1 px-2 rounded text-sm placeholder:text-zinc-400"
-                />
-              </div>
-            </div>
-            <footer className="flex justify-end mt-4">
-              <button
-                type="submit"
-                className="flex items-center gap-3 bg-blue-500 px-5 h-12 rounded-md font-semibold text-white"
-              >
-                Cadastrar
-              </button>
-            </footer>
           </form>
         </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/pages/Dashboard/RegSocialMedia.tsx
+++ src/pages/Dashboard/RegSocialMedia.tsx
@@ -1,125 +1,226 @@
-import { Typography } from '@mui/material'
+// import { zodResolver } from '@hookform/resolvers/zod'
+// import { useState } from 'react'
+// import { useForm } from 'react-hook-form'
+// import { z } from 'zod'
 
+/* const regDataUserSchema = z.object({
+  fullName: z.string(),
+  birth: z.date(),
+  email: z.string().email(),
+  contatc: z.string(),
+  maritalState: z.string(),
+  schoolDegree: z.string(),
+  genre: z.string(),
+  socialName: z.string().optional(),
+  cpf: z.string(),
+  rg: z.string(),
+  cnh: z.string(),
+  streetAdrress: z.string(),
+  city: z.string(),
+  country: z.string(),
+  cep: z.string(),
+})
+
+type RegDataUserFormInputs = z.infer<typeof regDataUserSchema>
+*/
 export function RegSocialMedia() {
+  /*
+  const { register, handleSubmit, watch } = useForm<RegDataUserFormInputs>({
+    resolver: zodResolver(regDataUserSchema),
+  })
+
+  function handleUserFormSubmit(data: RegDataUserFormInputs) {
+    console.log(data)
+  } */
+
   return (
-    <div>
-      <div className="flex flex-col w-[35%] m-auto mt-10">
-        <Typography className="font-bold text-center">
-          Cadastro de informações {'->'} Redes Sociais
-        </Typography>
-        <div className="flex w-full p-6 mb-4 bg-gray-400 rounded-lg border-b-2-cyan-500">
-          <form className="flex flex-col gap-3 w-full">
-            <div className="flex flex-col gap-2">
-              <label htmlFor="facebook" className="font-semibold">
-                Facebook
-              </label>
-              <input
-                type="text"
-                id="facebook"
-                name="facebook"
-                placeholder="facebook.com/usuario123"
-                className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="instagram" className="font-semibold">
-                Instagram
-              </label>
-              <input
-                type="text"
-                id="instagram"
-                name="instagram"
-                placeholder="instagram.com/usuario123"
-                className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="linkedin" className="font-semibold">
-                LinkedIn
-              </label>
-              <input
-                type="text"
-                id="linkedin"
-                name="linkedin"
-                placeholder="linkedin.com/usuario123"
-                className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="youtube" className="font-semibold">
-                Youtube
-              </label>
-              <input
-                type="text"
-                id="youtube"
-                name="youtube"
-                placeholder="youtube.com/@usuario123"
-                className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-              />
-            </div>
-            <div className="grid grid-cols-2 gap-4">
-              <div className="flex flex-col gap-2">
-                <label htmlFor="tiktok" className="font-semibold">
-                  TikTok
-                </label>
-                <input
-                  type="text"
-                  id="tiktok"
-                  name="tiktok"
-                  placeholder="tiktok.com/@usuario123"
-                  className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-                />
+    <>
+      <div className="md:grid md:grid-cols-3 md:gap-6 white py-4">
+        <div className="md:col-span-1">
+          <div className="px-4 sm:p-4">
+            <h3 className="text-lg font-bold leading-6 text-gray-800">
+              Cadastrar redes sociais
+            </h3>
+            <p className="mt-1 text-sm font-medium text-gray-700">
+              Confira os dados antes de cadastrar.
+              {/* <pre className="flex flex-wrap">
+                {JSON.stringify(watch(), null, 2)}
+              </pre> */}
+            </p>
+          </div>
+        </div>
+        <div className="mt-5 md:col-span-2">
+          <form>
+            <div className="overflow-hidden shadow sm:rounded-md mr-4">
+              <div className="bg-white px-4 py-5 sm:p-6">
+                <div className="grid grid-cols-6 gap-6 mx-auto">
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="instagram"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Instagram
+                    </label>
+                    <div className="flex rounded-md shadow-sm">
+                      <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
+                        http://instagram.com/
+                      </span>
+                      <input
+                        type="text"
+                        id="instagram"
+                        autoComplete="instagram"
+                        className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="facebook"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Facebook
+                    </label>
+                    <div className="flex rounded-md shadow-sm">
+                      <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
+                        http://facebook.com/
+                      </span>
+                      <input
+                        type="text"
+                        id="facebook"
+                        autoComplete="facebook"
+                        className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="linkedin"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Linkedin
+                    </label>
+                    <div className="flex rounded-md shadow-sm">
+                      <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
+                        http://linkedin.com/
+                      </span>
+                      <input
+                        type="text"
+                        id="linkedin"
+                        autoComplete="linkedin"
+                        className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="twitter"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Twitter
+                    </label>
+                    <div className="flex rounded-md shadow-sm">
+                      <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
+                        http://twitter.com/
+                      </span>
+                      <input
+                        type="text"
+                        id="twitter"
+                        autoComplete="twitter"
+                        className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="twitch"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Twitch
+                    </label>
+                    <div className="flex rounded-md shadow-sm">
+                      <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
+                        http://twitch.tv/
+                      </span>
+                      <input
+                        type="text"
+                        id="twitch"
+                        autoComplete="twitch"
+                        className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="youtube"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Youtube
+                    </label>
+                    <div className="flex rounded-md shadow-sm">
+                      <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
+                        http://youtube.com/
+                      </span>
+                      <input
+                        type="text"
+                        id="youtube"
+                        autoComplete="youtube"
+                        className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="facebook"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Tik Tok
+                    </label>
+                    <div className="flex rounded-md shadow-sm">
+                      <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
+                        https://www.tiktok.com/@
+                      </span>
+                      <input
+                        type="text"
+                        id="facebook"
+                        autoComplete="facebook"
+                        className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="facebook"
+                      className="block text-sm font-semibold text-gray-700"
+                    >
+                      Kwai
+                    </label>
+                    <div className="flex rounded-md shadow-sm">
+                      <input
+                        type="text"
+                        id="facebook"
+                        autoComplete="facebook"
+                        placeholder="@usuário"
+                        className="block w-full flex-1 rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col gap-2">
-                <label htmlFor="kwai" className="font-semibold">
-                  Kwai
-                </label>
-                <input
-                  type="text"
-                  id="kwai"
-                  name="kwai"
-                  placeholder="@usuario123"
-                  className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-                />
+
+              <div className="bg-zinc-50 px-4 py-3 text-right sm:px-6">
+                <button
+                  type="submit"
+                  className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                >
+                  Cadastrar
+                </button>
               </div>
             </div>
-            <div className="grid grid-cols-2 gap-4">
-              <div className="flex flex-col gap-2">
-                <label htmlFor="twitter" className="font-semibold">
-                  Twitter
-                </label>
-                <input
-                  type="text"
-                  id="twitter"
-                  name="twitter"
-                  placeholder="twitter.com/usuario123"
-                  className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <label htmlFor="twitch" className="font-semibold">
-                  Twitch
-                </label>
-                <input
-                  type="text"
-                  id="twitch"
-                  name="twitch"
-                  placeholder="twitter.com/usuario123"
-                  className="bg-zinc-900 py-1 px-2 rounded text-sm text-zinc-400 placeholder:text-zinc-400"
-                />
-              </div>
-            </div>
-            <footer className="flex justify-end mt-4">
-              <button
-                type="submit"
-                className="flex items-center gap-3 bg-blue-500 px-5 h-12 rounded-md font-semibold text-white"
-              >
-                Cadastrar
-              </button>
-            </footer>
           </form>
         </div>
       </div>
-    </div>
+    </>
   )
 }
